58eefbd5672ad29d2f4348e3df74d84c3b556456
Ashutosh Singh <ashutosh.as402@gmail.com>
2020-12-23 13:42:59 +0530

    Postgres supported first commit

---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+// Driver is struct which contain all the relevent field
+type Driver struct {
+	gorm.Model
+
+	Name    string
+	License string
+	Cars    []Car
+}
+
+// Car is struct which contain all the relevent field
+type Car struct {
+	gorm.Model
+
+	Year      int
+	Make      string
+	ModelName string
+	DriverID  int
+}
+
+var db *gorm.DB
+
+var err error
+
+var (
+	drivers = []Driver{
+		{Name: "Jimmy Johnson", License: "ABC123"},
+		{Name: "Howard Hills", License: "XYZ789"},
+		{Name: "Craig Colbin", License: "DEF333"},
+	}
+
+	cars = []Car{
+		{Year: 2000, Make: "Toyota", ModelName: "Tundra", DriverID: 1},
+		{Year: 2001, Make: "Honda", ModelName: "Accord", DriverID: 1},
+		{Year: 2002, Make: "Nissan", ModelName: "Sentra", DriverID: 2},
+		{Year: 2003, Make: "Ford", ModelName: "F-150", DriverID: 3},
+	}
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "Ashu88742@"
+	dbname   = "postgres_DB"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	router := mux.NewRouter()
+	db, err = gorm.Open("postgres", psqlInfo)
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Driver{})
+	db.AutoMigrate(&Car{})
+
+	for index := range cars {
+		db.Create(&cars[index])
+	}
+
+	for index := range drivers {
+		db.Create(&drivers[index])
+	}
+
+	router.HandleFunc("/cars", GetCars).Methods("GET")
+	router.HandleFunc("/cars/{id}", GetCar).Methods("GET")
+	router.HandleFunc("/drivers/{id}", GetDriver).Methods("GET")
+	router.HandleFunc("/cars/{id}", DeleteCar).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+
+}
+
+func GetCars(w http.ResponseWriter, r *http.Request) {
+	var cars []Car
+	db.Find(&cars)
+	json.NewEncoder(w).Encode(&cars)
+}
+
+func GetCar(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var car Car
+	db.First(&car, params["id"])
+	json.NewEncoder(w).Encode(&car)
+}
+
+func GetDriver(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var driver Driver
+	var cars []Car
+	db.First(&driver, params["id"])
+	db.Model(&driver).Related(&cars)
+	driver.Cars = cars
+	json.NewEncoder(w).Encode(&driver)
+}
+
+func DeleteCar(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var car Car
+	db.First(&car, params["id"])
+	db.Delete(&car)
+	var cars []Car
+	db.Find(&cars)
+	json.NewEncoder(w).Encode(&cars)
+}
